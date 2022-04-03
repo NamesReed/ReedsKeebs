773bba525bf64a75dc52546e23fc37e0f7633ff2
Dylan <36281259+NamesReed@users.noreply.github.com>
2022-04-03 17:02:56 -0600

    Replaced code.py with main.py
    improved default keymap, fixed encoder pins and direction.

--- Alish40 KMK Firmware/main.py
+++ Alish40 KMK Firmware/main.py
@@ -15,12 +15,12 @@ layers_ext = Layers()
 keyboard.modules = [layers_ext, encoder_handler]
 
 #Alish Pins for KB2040
-keyboard.col_pins = (board.D0, board.D1, board.D2, board.D3, board.D4, board.D5, board.MOSI, board.D10, board.D9, board.D8, board.D7, board.D6)
+keyboard.col_pins = (board.D0, board.D1, board.D2, board.D3, board.D4, board.D5, board.D10, board.MOSI, board.D9, board.D8, board.D7, board.D6)
 keyboard.row_pins = (board.A1, board.A0, board.SCK, board.MISO)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 #Alish Encoder Pins for KB2040
-encoder_handler.pins = ((board.A2, board.A3, board.A4, False),)
+encoder_handler.pins = ((board.A3, board.A2, board.SDA, False),)
 
 
 keyboard.keymap = [
@@ -32,12 +32,12 @@ keyboard.keymap = [
     ],
     [#Number Layer
         KC.ESC, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.BSPC, 
-	    KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, 
+	    KC.DEL, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.EQL, KC.MINS, KC.LBRC, KC.RBRC, KC.SCLN, 
 	    KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.SLASH, KC.TRNS, 
 	    KC.LCTL, KC.TRNS, KC.LALT, KC.NO, KC.TRNS, KC.NO, KC.SPC, KC.NO, KC.LCTL, KC.LEFT, KC.RGHT,
     ],
     [#Function Layer
-        KC.ESC, KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.F6, KC.F7, KC.F8, KC.F9, KC.VOLD, KC.VOLU, 
+        KC.ESC, KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, 
 	    KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, 
 	    KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, 
 	    KC.LCTL, KC.TRNS, KC.LALT, KC.NO, KC.TRNS, KC.NO, KC.NO, KC.SPC, KC.NO, KC.LCTL, KC.LEFT, KC.RGHT,
